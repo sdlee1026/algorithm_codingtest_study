19ef2a0842d4344ee8a15be904d89dff5d347587
sd_Lee <leesungdae@skuniv.ac.kr>
2022-12-09 18:59:58 +0900

    FIXED Typo

--- cpp/algorithm/codesignal/arcade/codesignal_arcade_IslandOfKnowledge.cpp
+++ cpp/algorithm/codesignal/arcade/codesignal_arcade_IslandOfKnowledge.cpp
@@ -239,7 +239,7 @@ vector<vector<int>> solution_BoxBlur(vector<vector<int>> image)
 	return answer;
 }
 
-	/*
+/*
 	In the popular Minesweeper game you have a board with some mines and those cells that
 	don't contain a mine have a number in it that indicates the total number of mines in the neighboring cells.
 	Starting off with some arrangement of mines we want to create a Minesweeper game setup.
