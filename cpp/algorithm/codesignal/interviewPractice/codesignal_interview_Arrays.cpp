7e95b29e8ccc306c56d93fba9fc1b3b239b64f77
sd_Lee <leesungdae@skuniv.ac.kr>
2023-01-05 14:50:44 +0900

    UPDATE 230105, interview Practice Arrays

--- cpp/algorithm/codesignal/interviewPractice/codesignal_interview_Arrays.cpp
+++ cpp/algorithm/codesignal/interviewPractice/codesignal_interview_Arrays.cpp
@@ -113,3 +113,104 @@ vector<vector<int>> solution_rotateImage(vector<vector<int>> a)
     }
     return v;
 }
+
+/*
+
+    Sudoku is a number-placement puzzle. The objective is to fill a 9 ¡¿ 9 grid with numbers in such a way that each column,
+    
+    each row, and each of the nine 3 ¡¿ 3 sub-grids that compose the grid all contain all of the numbers from 1 to 9 one time.
+
+    Implement an algorithm that will check whether the given grid of numbers represents a valid Sudoku puzzle according to the layout rules described above.
+    
+    Note that the puzzle represented by grid does not have to be solvable.
+
+    Example
+
+    For
+
+    grid = [['.', '.', '.', '1', '4', '.', '.', '2', '.'],
+            ['.', '.', '6', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '1', '.', '.', '.', '.', '.', '.'],
+            ['.', '6', '7', '.', '.', '.', '.', '.', '9'],
+            ['.', '.', '.', '.', '.', '.', '8', '1', '.'],
+            ['.', '3', '.', '.', '.', '.', '.', '.', '6'],
+            ['.', '.', '.', '.', '.', '7', '.', '.', '.'],
+            ['.', '.', '.', '5', '.', '.', '.', '7', '.']]
+    the output should be
+    solution(grid) = true;
+
+    For
+
+    grid = [['.', '.', '.', '.', '2', '.', '.', '9', '.'],
+            ['.', '.', '.', '.', '6', '.', '.', '.', '.'],
+            ['7', '1', '.', '.', '7', '5', '.', '.', '.'],
+            ['.', '7', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '8', '3', '.', '.', '.'],
+            ['.', '.', '8', '.', '.', '7', '.', '6', '.'],
+            ['.', '.', '.', '.', '.', '2', '.', '.', '.'],
+            ['.', '1', '.', '2', '.', '.', '.', '.', '.'],
+            ['.', '2', '.', '.', '3', '.', '.', '.', '.']]
+    the output should be
+    solution(grid) = false.
+
+    The given grid is not correct because there are two 1s in the second column. Each column, each row, and each 3 ¡¿ 3 subgrid can only contain the numbers 1 through 9 one time.
+
+    Input/Output
+
+    [execution time limit] 0.5 seconds (cpp)
+
+    [input] array.array.char grid
+
+    A 9 ¡¿ 9 array of characters, in which each character is either a digit from '1' to '9' or a period '.'.
+
+    [output] boolean
+
+    Return true if grid represents a valid Sudoku puzzle, otherwise return false.
+
+*/
+// https://app.codesignal.com/interview-practice/question/SKZ45AF99NpbnvgTn/description
+
+bool solution_sudoku2(vector<vector<char>> grid)
+{
+    for (auto row : grid)
+    {
+        vector<int> rowCheck(9, 0);
+        for (auto n : row)
+            if (n != '.')
+                rowCheck[int(n) - 49]++;
+        for (auto n : rowCheck)
+            if (n > 1)
+                return false;
+    }
+    for (int i = 0; i < grid.size(); ++i)
+    {
+        vector<int> colCheck(9, 0);
+        for (int j = 0; j < grid.size(); ++j)
+            if (grid[j][i] != '.')
+                colCheck[int(grid[j][i] - 49)]++;
+        for (auto n : colCheck)
+            if (n > 1)
+                return false;
+    }
+    for (int i = 0; i < grid.size(); i += 3)
+        for (int j = 0; j < grid.size(); j += 3)
+        {
+            vector<int> check(9, 0);
+            if (grid[i][j] != '.')      check[int(grid[i][j]) - 49]++;
+            if (grid[i][j + 1] != '.')    check[int(grid[i][j + 1]) - 49]++;
+            if (grid[i][j + 2] != '.')    check[int(grid[i][j + 2]) - 49]++;
+            if (grid[i + 1][j] != '.')    check[int(grid[i + 1][j]) - 49]++;
+            if (grid[i + 1][j + 1] != '.')  check[int(grid[i + 1][j + 1]) - 49]++;
+            if (grid[i + 1][j + 2] != '.')  check[int(grid[i + 1][j + 2]) - 49]++;
+            if (grid[i + 2][j] != '.')    check[int(grid[i + 2][j]) - 49]++;
+            if (grid[i + 2][j + 1] != '.')  check[int(grid[i + 2][j + 1]) - 49]++;
+            if (grid[i + 2][j + 2] != '.')  check[int(grid[i + 2][j + 2]) - 49]++;
+            for (auto n : check)
+                if (n > 1)
+                    return false;
+            check.clear();
+        }
+
+    return true;
+}
--- cpp/algorithm/codesignal/interviewPractice/codesignal_interview_Arrays.h
+++ cpp/algorithm/codesignal/interviewPractice/codesignal_interview_Arrays.h
@@ -3,3 +3,4 @@
 int solution_firstDuplicate(vector<int> a);
 char solution_firstNotRepeatingCharacter(string s);
 vector<vector<int>> solution_rotateImage(vector<vector<int>> a);
+bool solution_sudoku2(vector<vector<char>> grid);
